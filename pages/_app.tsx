5ffdaeebc271721f12d8c0c03443dbe4199c823d
muhammadariefhidayat <tubagusarief45@gmail.com>
2023-02-01 21:09:35 +0700

    interactif porto

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import "tailwindcss/tailwind.css";
-import "@/styles/main.css";
-
-import { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
--- types/index.ts
+++
@@ -1,9 +0,0 @@
-export type Project = {
-  id: number;
-  title: string;
-  desc: string;
-  img: string;
-  link?: string;
-  github?: string;
-  tags: string[];
-};
