e393b67d8092672a122e4e12213c282262908a01
muhammadariefhidayat <tubagusarief45@gmail.com>
2023-02-01 20:47:59 +0700

    adding porto

---
+++ data/global.ts
@@ -0,0 +1,113 @@
+type Route = {
+  title: string,
+  path: string
+}
+
+type FooterCol = {
+  title: string,
+  links: {
+    name: string,
+    link: string,
+    icon?: string,
+    leavesWebsite: boolean
+  }[]
+}
+
+type Footer = {
+  columns: FooterCol[]
+  support: {
+    buymeacoffee: string
+    paypal: string
+    message: string
+  }
+};
+
+export const routes: Route[] = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  // {
+  //   title: "Blog",
+  //   path: "/blog",
+  // },
+  {
+    title: "Projects",
+    path: "/projects",
+  },
+  {
+    title: "Designs",
+    path: "/designs",
+  },
+];
+
+
+export const footer: Footer = {
+  columns: [
+    {
+      title: "Pages",
+      links: [
+        {
+          name: "Home",
+          link: "/",
+          leavesWebsite: false,
+        },
+        // {
+        //   name: "Blog",
+        //   link: "/blog",
+        //   leavesWebsite: false,
+        // },
+        {
+          name: "Projects",
+          link: "/projects",
+          leavesWebsite: false,
+        },
+        {
+          name: "Designs",
+          link: "/designs",
+          leavesWebsite: false,
+        },
+      ],
+    },
+    {
+      title: "Social",
+      links: [
+        {
+          name: "GitHub",
+          link: "https://github.com/braydentw",
+          icon: "/static/icons/github-f.svg",
+          leavesWebsite: true,
+        },
+        {
+          name: "LinkedIn",
+          link: "https://www.linkedin.com/in/braydentw/",
+          icon: "/static/icons/linkedin-f.svg",
+          leavesWebsite: true,
+        },
+        {
+          name: "Dribbble",
+          link: "https://dribbble.com/braydentw",
+          icon: "/static/icons/dribbble-f.svg",
+          leavesWebsite: true,
+        },
+        {
+          name: "IndieHackers",
+          link: "https://indiehackers.com/braydentw",
+          icon: "/static/icons/indiehackers-f.svg",
+          leavesWebsite: true,
+        },
+        {
+          name: "Email",
+          link: "mailto:contact@braydentw.io",
+          icon: "/static/icons/mail-f.svg",
+          leavesWebsite: true,
+        },
+      ],
+    },
+  ],
+  support: {
+    buymeacoffee: "braydenw",
+    paypal: "braydentw",
+    message: "I appreciate your support very much! 💙",
+  },
+};
