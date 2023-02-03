aac2777cb26cce2b22c32ad708234cee44a6f46a
muhammadariefhidayat <tubagusarief45@gmail.com>
2023-02-04 03:50:47 +0700

    80

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
